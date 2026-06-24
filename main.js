af164cc2887677da1e32722d4608981c278b9d4f
NafNafra <nafafyandrianaivo@gmail.com>
2026-06-24 12:50:59 +0300

    basic parallelepipede

---
+++ main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+
+// Essectials : Camera, scene, renderer
+const scene = new THREE.Scene();
+
+//(field of view, aspect ratio, near, far)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(1 ,2 , 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const parallelo = new THREE.Mesh(geometry, material)
+
+scene.add(parallelo)
+
+camera.position.z = 5
+
+
+function animate( time ) {
+  parallelo.rotation.x = time / 2000;
+  parallelo.rotation.y = time / 1000;
+  renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
